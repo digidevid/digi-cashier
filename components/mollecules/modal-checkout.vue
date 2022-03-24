2d505fb9fe7a9b48d8a3d77f0a81bd55396274e2
Ibnu Hazar <ibnuhazar101@gmail.com>
2022-03-25 04:11:02 +0700

    first commit

---
+++ components/mollecules/modal-checkout.vue
@@ -0,0 +1,125 @@
+<template>
+  <div
+    v-if="isShow"
+    tabindex="0"
+    class="z-40 overflow-auto left-0 top-0 bottom-0 right-0 w-full h-full fixed"
+  >
+    <div class="z-50 relative mx-auto my-0 w-full">
+      <div
+        class="modal-popup w-full md:w-3/4 lg:w-1/2 xl:w-1/3 flex flex-col overflow-hidden rounded-3xl px-4 pt-8 pb-6 bg-white"
+      >
+        <button
+          class="fill-current h-6 w-6 absolute right-3 top-3 font-3xl font-bold primary focus:outline-none"
+          @click="onClose"
+        >
+          <svg fill="currentColor" viewBox="0 0 20 20" class="w-6 h-6 primary">
+            <path
+              fill-rule="evenodd"
+              d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+              clip-rule="evenodd"
+            ></path>
+          </svg>
+        </button>
+        <div class="scroll-bar !overflow-y-auto !overscroll-auto md:px-2">
+          <div>
+            <h3 class="text-2xl font-bold text-center mb-2">Checkout</h3>
+            <div
+              class="flex justify-between items-center text-xl font-semibold"
+            >
+              <p>Pesanan</p>
+              <p>Harga</p>
+            </div>
+            <div
+              v-for="(item, id) in checkoutMenus"
+              :key="id"
+              class="flex justify-between items-center border-b-2 border-dashed pb-1 space-y-1 mt-2"
+            >
+              <div>
+                <p>{{ item.name }}</p>
+                <p class="text-sm">
+                  {{ toRupiah(item.price) }} x {{ item.quantity }}
+                </p>
+              </div>
+              <p class="text-lg">{{ toRupiah(item.totalPrice) }}</p>
+            </div>
+            <div
+              class="flex justify-between items-center my-3 text-xl font-semibold"
+            >
+              <p>TOTAL</p>
+              <p>{{ toRupiah(total) }}</p>
+            </div>
+            <div>
+              <button
+                class="bg-blue-500 w-full py-3 text-lg font-semibold text-white rounded-md"
+                @click="submitData"
+                :disabled="isLoading"
+              >
+                <div v-if="isLoading">
+                  <font-awesome-icon :icon="['fa', 'circle-o-notch', 'spin']" />
+                  Loading...
+                </div>
+                <div v-else>Submit</div>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div
+      class="z-40 overflow-auto left-0 top-0 bottom-0 right-0 w-full h-full fixed bg-black opacity-50"
+      @click="onClose"
+    ></div>
+  </div>
+</template>
+
+<script>
+import { toRupiah } from "~/helper/currency";
+export default {
+  props: {
+    isLoading: {
+      type: Boolean,
+      default: false,
+    },
+    isShow: {
+      type: Boolean,
+      default: false,
+    },
+    checkoutMenus: {
+      type: Array,
+      default: () => [],
+    },
+    total: {
+      type: Number,
+      default: 0,
+    },
+  },
+  methods: {
+    onClose() {
+      this.$emit("onClose");
+    },
+    submitData() {
+      this.$emit("submitData", this.checkoutMenus);
+    },
+    toRupiah,
+  },
+};
+</script>
+
+<style>
+.modal-popup {
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  max-height: 95%;
+  max-width: 95%;
+  min-width: 20rem;
+}
+.scroll-bar::-webkit-scrollbar {
+  display: none;
+}
+.scroll-bar {
+  -ms-overflow-style: none; /* IE and Edge */
+  scrollbar-width: none; /* Firefox */
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,163 @@
+<template>
+  <div class="bg-blue-100 max-w-lg mx-auto">
+    <div class="p-8">
+      <h1 class="text-center text-3xl font-bold my-4">Kasir DigiCafe</h1>
+      <div>
+        <h3 class="text-xl font-bold mb-2">Foods</h3>
+        <div v-for="(food, id) in foods" :key="id">
+          <div
+            class="flex justify-between items-center border-b-2 border-dashed border-yellow-600 p-2"
+          >
+            <p>{{ food.name }}</p>
+            <div class="flex space-x-3 items-center">
+              <button @click="minus(food)" :disabled="food.quantity === 0">
+                <font-awesome-icon
+                  :icon="['fa', 'circle-minus']"
+                  class="text-red-400"
+                />
+              </button>
+              <p>{{ food.quantity }}</p>
+              <button @click="plus(food)">
+                <font-awesome-icon
+                  :icon="['fa', 'circle-plus']"
+                  class="text-green-400"
+                />
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div>
+        <h3 class="text-xl font-bold mt-4 mb-2">Drinks</h3>
+        <div v-for="(drink, id) in drinks" :key="id">
+          <div
+            class="flex justify-between items-center border-b-2 border-dashed border-yellow-600 p-2"
+          >
+            <p>{{ drink.name }}</p>
+            <div class="flex space-x-3 items-center">
+              <button @click="minus(drink)" :disabled="drink.quantity === 0">
+                <font-awesome-icon
+                  :icon="['fa', 'circle-minus']"
+                  class="text-red-400"
+                />
+              </button>
+              <p>{{ drink.quantity }}</p>
+              <button @click="plus(drink)">
+                <font-awesome-icon
+                  :icon="['fa', 'circle-plus']"
+                  class="text-green-400"
+                />
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="sticky bottom-0 bg-white rounded-t-2xl">
+      <div
+        class="pt-4 pb-2 px-6 flex justify-between items-center text-xl font-bold"
+      >
+        <p>TOTAL</p>
+        <p>{{ toRupiah(total) }}</p>
+      </div>
+      <div class="p-2">
+        <button
+          class="bg-blue-500 w-full py-3 text-lg font-semibold text-white rounded-md"
+          @click="checkout"
+        >
+          Checkout
+        </button>
+      </div>
+    </div>
+    <ModalCheckout
+      :is-show="isShowModalCheckout"
+      :checkout-menus="checkoutItems"
+      :total="total"
+      :is-loading="isLoadingSubmit"
+      @onClose="onCloseModalCheckout"
+      @submitData="submitData"
+    />
+  </div>
+</template>
+
+<script>
+import ModalCheckout from "../components/mollecules/modal-checkout.vue";
+import { foods, drinks } from "../constants/menu";
+import { toRupiah } from "../helper/currency";
+export default {
+  name: "IndexPage",
+  components: {
+    ModalCheckout,
+  },
+  data() {
+    return {
+      foods,
+      drinks,
+      total: 0,
+      checkoutItems: [],
+      isShowModalCheckout: false,
+      isLoadingSubmit: false,
+    };
+  },
+  methods: {
+    minus(item) {
+      item.quantity--;
+      item.totalPrice = item.price * item.quantity;
+      this.calculate();
+    },
+    plus(item) {
+      item.quantity++;
+      item.totalPrice = item.price * item.quantity;
+      this.calculate();
+    },
+    calculate() {
+      const totalFoodPrice = this.foods
+        .map((item) => item.totalPrice)
+        .reduce((prev, curr) => prev + curr, 0);
+      const totalDrinkPrice = this.drinks
+        .map((item) => item.totalPrice)
+        .reduce((prev, curr) => prev + curr, 0);
+      this.total = totalFoodPrice + totalDrinkPrice;
+    },
+    checkout() {
+      const allMenu = this.foods.concat(this.drinks);
+      this.checkoutItems = allMenu.filter((item) => item.quantity > 0);
+      this.isShowModalCheckout = true;
+    },
+    onCloseModalCheckout() {
+      this.isShowModalCheckout = false;
+    },
+    async submitData(data) {
+      this.isLoadingSubmit = true;
+      const scriptUrl =
+        "https://script.google.com/macros/s/AKfycbyQRHwlWM_dvlXiMNX2EYKX3HkGSmN6kh13efYk_B_gYjBhTJPoW5QNseS9w_SwaSAu/exec";
+
+      await data.forEach((el) => {
+        const newForm = new FormData();
+        const today = new Date();
+
+        newForm.append("Tanggal", today.toDateString());
+        newForm.append("Nama", el.name);
+        newForm.append("Harga", el.price);
+        newForm.append("Jumlah", el.quantity);
+        newForm.append("Total", el.totalPrice);
+
+        fetch(scriptUrl, {
+          method: "POST",
+          body: newForm,
+          changeOrigin: true,
+        })
+          .then((response) => {
+            console.log("Success!", response);
+          })
+          .catch((error) => {
+            console.error("Error!", error.message);
+          });
+      });
+      this.isShowModalCheckout = false;
+      this.isLoadingSubmit = false;
+    },
+    toRupiah,
+  },
+};
+</script>
